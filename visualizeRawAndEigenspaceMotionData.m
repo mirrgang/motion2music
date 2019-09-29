b3e42e08f0ae4308bce69a2d06e24582c81313ac
mirrgang <melanie.irrgang@io-pc-technik.de>
2019-09-29 18:36:54 +0200

    raw, pre-processed, eigenspace, data final touch

--- visualizeRawAndEigenspaceMotionData.m
+++ visualizeRawAndEigenspaceMotionData.m
@@ -1,6 +1,7 @@
-raw = csvread(fullfile('/home/melanie/WORKSPACE/motion2music2017/motion_phone/','LeavingSong_#12_acc.csv'));
-pre_processed = load('motion_data_pre_processed/LeavingSong_#12_.mat');
-eigenspace = load('motion_data_in_eigenspace/LeavingSong_#12_.mat');
+close all;
+raw = csvread(fullfile('/home/melanie/WORKSPACE/motion2music2017/motion_phone/','AladdinSane_#8_acc.csv'));
+pre_processed = load('motion_data_pre_processed/AladdinSane_#8_.mat');
+eigenspace = load('motion_data_in_eigenspace/AladdinSane_#8_.mat');
 durationInSeconds = 20;
 N = size(raw,1);
 x_unit = durationInSeconds/N;
@@ -14,34 +15,43 @@ xticksPre = 5:x_unit:durationInSeconds-x_unit;
 figure
 subplot(3,1,1)
 hold on
-plot(xticks, raw(:,1));
-plot(xticks, raw(:,2));
-plot(xticks, raw(:,3));
-legend('x1','x2','x3');
+plot(xticks, raw(:,1), 'k-');
+r1 = plot(xticks, raw(:,2), '.-');
+r2 = plot(xticks, raw(:,3), ':');
+legend('x','y','z');
 xlabel('time in s');
 ylabel('m/s²');
 ylim([-40 40]);
+xlim([0 20])
+set(r1, 'color', [0.6 0.6 0.6]);
+set(r2, 'color', [0.2 0.2 0.2]);
 title('original accelerometer data')
 hold off
 subplot(3,1,2)
 hold on
-plot(xticksPre, pre_processed.data(:,1));
-plot(xticksPre, pre_processed.data(:,2));
-plot(xticksPre, pre_processed.data(:,3));
-legend('x1','x2','x3');
+plot(xticksPre, pre_processed.data(:,1), 'k-');
+p1 = plot(xticksPre, pre_processed.data(:,2), '.-');
+p2 = plot(xticksPre, pre_processed.data(:,3), ':');
+legend('x','y','z');
 xlabel('time in s');
 ylabel('m/s²');
 ylim([-40 40]);
+xlim([0 20])
+set(p1, 'color', [0.6 0.6 0.6]);
+set(p2, 'color', [0.2 0.2 0.2]);
 title('pre-processed accelerometer data')
 subplot(3,1,3)
 hold on
-plot(xticksPre, eigenspace.data(:,1));
-plot(xticksPre, eigenspace.data(:,2));
-plot(xticksPre, eigenspace.data(:,3));
+plot(xticksPre, eigenspace.data(:,1), 'k-');
+e1 = plot(xticksPre, eigenspace.data(:,2), '.-');
+e2 = plot(xticksPre, eigenspace.data(:,3), ':');
 legend('PC1', 'PC2', 'PC3');
 xlabel('time in s');
 ylabel('m/s²');
 ylim([-40 40]);
+xlim([0 20])
+set(e1, 'color', [0.6 0.6 0.6]);
+set(e2, 'color', [0.2 0.2 0.2]);
 title('pre-processed accelerometer data in eigenspace')
 
 %###############################################
